258f8fff33678308aaeb5fac729ef90eb83ae723
evgendv777 <evgendv777@gmail.com>
2022-09-04 12:07:20 +0300

    last

--- components/Comments.jsx
+++ components/Comments.jsx
@@ -20,16 +20,16 @@ function Comments({ slug }) {
           <h3 className="text-xl mb-8 font-semibold border-b pb-4">
             {comments.length}
             {' '}
-            Comments
+            Комментарий
           </h3>
             {comments.map((comment, index) => (
               <div key={index} className="border-b border-gray-100 mb-4 pb-4">
                 <p className="mb-4">
                   <span className="font-semibold">{comment.name}</span>
                   {' '}
-                  on
+                  от
                   {' '}
-                  {moment(comment.createdAt).format('MMM DD, YYYY')}
+                  {moment(comment.createdAt).format('DD MMM , YYYY')}
                 </p>
                 <p className="whitespace-pre-line text-gray-600 w-full">{parse(comment.comment)}</p>
               </div>

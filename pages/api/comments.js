650347ab28cf739bd2e67eaf88d679c4104bdee8
evgendv777 <evgendv777@gmail.com>
2022-09-04 11:36:00 +0300

    1111

--- pages/api/comments.js
+++ pages/api/comments.js
@@ -22,7 +22,7 @@ export default async function handler(req, res) {
     const result = await graphQLClient.request(query, req.body);
     return res.status(200).send(result);
   } catch (error) {
-    console.log(error);
+    // console.log(error);
     return res.status(500).send(error);
   }
 }
